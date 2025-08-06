f2cb429d70c940f8b354aaf24ae3837cba9276e8
Sithumina0027 <40-bit-0027@kdu.ac.lk>
2025-08-06 13:01:47 +0530

    first commit

---
+++ 01.py
@@ -0,0 +1,46 @@
+"""
+print("Welcome to pythan")
+print(7+3)
+print(type(6.66))
+print(10%2==0)
+print("AI"+"Chatbot")
+
+i=20
+if(i<15):
+    print("i is less than 15")
+    print("I'm in if Block")
+else:
+    print("i is greater than to 15")
+    print("I'm in else Block")
+    print("I'm not in if and else Block")
+    
+i=20
+if(i==10):
+    print("i is 10")
+elif(i==15):
+    print("i is 15")
+elif(i==20):
+    print("i is 20")
+else:
+    print("i is not present")    
+    
+i=1
+while i<6:
+    print(i)
+    i+=1  
+    
+fruits=["apple","banana","cherry"]
+for x in fruits:
+    print(x) 
+
+marks=int(input("Enter your marks: "))
+print(type(marks))  
+
+marks1=int(input("Enter your marks: "))
+marks2=int(input("Enter your marks: "))
+marks3=int(input("Enter your marks: "))
+total=marks1+marks2+marks3
+print("Total marks are: ", total) 
+
+"""
+
